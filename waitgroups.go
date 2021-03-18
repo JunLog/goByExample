d6a3a423b583a1af06244a37e60a6db0fa2fdb2e
Zhang JunLong <JunLong.Zhang@vatoptec.com>
2021-03-18 13:55:50 +0800

    添加 WaitGroup 的作用说明与使用步骤

--- waitgroups.go
+++ waitgroups.go
@@ -6,6 +6,14 @@ import (
 	"time"
 )
 
+// WaitGroup 主要避免 goroutine 泄露
+
+// 在每个 goroutine 启动前增加一个计数，同时在每一个 goroutine 结束时递减计数
+
+// Add(delta int)：增加/减少若干计数
+// Done：减少 1 个计数，等价于 Add(-1)
+// Wait：卡住，直到计数等于 0
+
 // 每个协程都调用这个函数，WaitGroup 必须通过指针传递
 func worker(id int, wg *sync.WaitGroup) {
 	defer wg.Done() // 最后通知 WaitGroup,当前协程已经完成
